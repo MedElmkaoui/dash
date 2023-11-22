edceeb3d39ecaa49c8971048f87971255e1c586a
MedElmkaoui <MedElmkaoui@gmail.com>
2023-11-22 21:36:57 +0100

    22/11/2023 21:30

--- components/Forms/RowForm.tsx
+++ components/Forms/RowForm.tsx
@@ -5,7 +5,7 @@ export type RowFormProps = {
 
 function RowForm({children}: RowFormProps) {
   return (
-    <div className="mb-4.5 flex flex-col gap-6 xl:flex-row"> 
+    <div className="mb-4.5 flex justify-center  flex-col gap-6 xl:flex-row xl:items-end"> 
         {children}
     </div>
   )
